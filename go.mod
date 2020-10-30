fb26d89a43a79a0d776b1485c883a78dd83fc78d
ada-u <yuta@adachi.life>
2020-10-30 17:45:30 +0900

    Listen on 2112

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-project
+
+go 1.15
+
+require github.com/prometheus/client_golang v1.8.0 // indirect
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
+	"log"
+	"net/http"
+)
+
+const (
+	namespace    = "SendGrid"
+	exporterName = "sendgrid-stats-exporter"
+)
+
+const (
+	port = 2112
+)
+
+type statsCollector struct{}
+
+func main() {
+	log.Printf("Starting %s %s\n", exporterName, version.Info())
+	log.Printf("Build context %s\n", version.BuildContext())
+
+	http.Handle("/metrics", promhttp.Handler())
+	log.Printf("Listening on %d", port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
+		log.Fatal(err)
+	}
+}
