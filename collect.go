506c55ae9f93e49e452ba6edb8d3fc5c0fe6210f
cw-sakamoto <sakamoto@chatwork.com>
2022-05-30 09:09:08 +0900

    bump golang and library

--- collect.go
+++ collect.go
@@ -3,10 +3,10 @@ package main
 import (
 	"time"
 
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/log/level"
-	"github.com/prometheus/client_golang/prometheus"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/jinzhu/now"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Collector struct {
